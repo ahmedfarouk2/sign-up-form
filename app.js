196fad684ea50912e4ca849074edad681280d37a
ahmedfarouk2 <ahmedoza12@gmail.com>
2023-02-08 07:36:08 +0200

    JS wired for validating passwords

---
+++ app.js
@@ -0,0 +1,12 @@
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirm-password');
+const submitButton = document.querySelector('#sign-up');
+
+submitButton.addEventListener('click', (e) => {
+    if (passwordInput.value !== confirmPasswordInput.value){
+        confirmPasswordInput.setCustomValidity(`Passwords don't match bro, Try matching them and you won't see this message!`)
+    }
+    else {
+        confirmPasswordInput.setCustomValidity('');
+    }
+})
